be1e3e8a5102240de8ec1ffd4ef05ddc162142d1
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-05-28 19:14:58 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
+btnStop.setAttribute('disabled', '');
+// color changing function
+function colorChangefct() {
+  const colorChange = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+// color change stop
 
+// color generating function
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', '');
+  btnStop.removeAttribute('disabled');
+});
+btnStop.addEventListener('click', () => {
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', '');
+
+  clearInterval(timerId);
+});
