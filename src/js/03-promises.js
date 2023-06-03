84e95b2fdb9cc2644799ccfb3586e89525d75ca9
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-06-03 13:54:26 +0200

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+const form = document.querySelector('.form');
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function eventHandler(e) {
+  e.preventDefault();
+  const firstDelay = Number(delay.value);
+  const delayStep = Number(step.value);
+  const promiseAmount = Number(amount.value);
+
+  for (let i = 0; i < promiseAmount; i++) {
+    createPromise(i + 1, firstDelay + i * delayStep)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+form.addEventListener('submit', eventHandler);
